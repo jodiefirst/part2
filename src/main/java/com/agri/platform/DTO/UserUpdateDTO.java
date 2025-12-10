4d0267fd4adb5852299010a806235630f8cd14f1
Kaze <zhaofangming@mail.nwpu.edu.cn>
2025-12-10 23:53:01 +0800

    编写了部分测试类

--- src/main/java/com/agri/platform/DTO/UserUpdateDTO.java
+++ src/main/java/com/agri/platform/DTO/UserUpdateDTO.java
@@ -1,15 +1,9 @@
 package com.agri.platform.DTO;
 
-import java.time.LocalDateTime;
-
 public record UserUpdateDTO(
         String userId,
         String username,
         String phoneNumber,
-        String email,
-        String accountStatus,
-        LocalDateTime lastLoginTime,
-        String lastLoginIp,
-        Integer loginFailCount,
-        LocalDateTime loginLockedUntil) {
+        String email
+        ) {
 }
